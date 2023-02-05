243a2cce9b948758ce63c6f51bfd5fc1286c4883
Tio Bagio <10209203+tiobagio@users.noreply.github.com>
2023-02-05 15:06:40 +0800

    Update run-api.sh

--- run-api.sh
+++ run-api.sh
@@ -11,6 +11,6 @@ ERROR_RATE=0.6 ERROR_CODE=200 ERROR_TYPE=delay ERROR_DELAY=5s \
    fake-service &
 
 #TIMING_50_PERCENTILE=5s TIMING_90_PERCENTILE=30s TIMING_VARIANCE=150 \
-    NAME=api-v1 MESSAGE="IM API V1" LISTEN_ADDR='0.0.0.0:6061' \
-    LOG_OUTPUT=/tmp/api1.log LOG_LEVEL=debug \
-    fake-service &
+#    NAME=api-v1 MESSAGE="IM API V1" LISTEN_ADDR='0.0.0.0:6061' \
+#    LOG_OUTPUT=/tmp/api1.log LOG_LEVEL=debug \
+#    fake-service &
